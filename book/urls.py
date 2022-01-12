24512fdc4e6d709f7235159465aa42fa82176a0a
user <nurikszy@gmail.com>
2022-01-12 18:28:55 +0600

    HW2 month3

--- book/urls.py
+++ book/urls.py
@@ -2,5 +2,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('books/', views.book_all, name='book'),
+    path('books/', views.book_all, name='books_all'),
+    path('books/<int:id>/', views.books_detail, name='book_detail'),
 ]
--- book/views.py
+++ book/views.py
@@ -1,7 +1,10 @@
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from . import models
-
 def book_all(requests):
     book = models.Book.objects.all()
     return render(requests, 'book_list.html', {'book': book})
+
+def books_detail(request, id):
+    book = get_object_or_404(models.Book, id=id)
+    return render(request, 'books_details.html', {'book': book})
