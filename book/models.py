41442577afa0c9b2e5a5ced855f957a7beb2f023
user <nurikszy@gmail.com>
2022-01-17 21:51:15 +0600

    HW3 month3

--- book/models.py
+++ book/models.py
@@ -7,7 +7,7 @@ class Book(models.Model):
     description = models.TextField()
     image = models.ImageField(upload_to='')
     release_date = models.DateTimeField(auto_now_add=True)
-    update_date = models.DateTimeField(auto_now_=True)
+    update_date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f'{self.name}'
